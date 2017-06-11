3b5488160ac507c354fa7d112a6de1c71dcead19
RBigley <wx8ball@gmail.com>
2017-06-10 23:43:39 -0600

    feat: Randomize OtherChoreList

--- program/chores.py
+++ program/chores.py
@@ -33,16 +33,34 @@ def addAllChores():
             	k[rnum].append(row[0])
     return k
 
-def addOtherChores():
-    dayOfChore = random.sample(range(7), k = otherChore['weeklyFrequency'])
-    minAge = otherChore['minAge']
-    if otherChore['dayofWeek'] != '*':
-        for dayOfChore in otherChore['dayofWeek'].split(';'):
-            helperGroup = helpers.query("age >= @minAge")
+def addOtherChores(row):
+    dayOfChore = random.sample(range(7), k = row['weeklyFrequency'])
+    minAge = row['minAge']
+    c = row['chore']
+    helperGroup = helpers.query('age >= @minAge')
+    helperGroup.reset_index()
+    helpersCount = helperGroup['age'].count()
+    wchore = [[] for i in range(7)]
+    if row['dayofWeek'] != '*':
+        rsplit = row['dayofWeek'].split(';')
+        for dChore in rsplit:
+            helpRandom = random.randint(0,helpersCount-1)
+            dChore = int(dChore)
+            wchore[dChore] = helperGroup['helper'][helpRandom]  
+    elif row['weeklyFrequency'] == 7:
+        for i in range(7):
+            helpRandom = random.randint(0,helpersCount-1)
+            wchore[i] = helperGroup['helper'][helpRandom]
+    else:
+        for i in dayOfChore:
+            helpRandom = random.randint(0,helpersCount-1)
+            wchore[i] = helperGroup['helper'][helpRandom]
+    return wchore
 
 for j in helpers['helper']:
     t[j] = addAllChores()
+
     #addOtherChores()
     #t[j].update(addOtherChores())
 
-print(t)
+print(otherChore.apply(addOtherChores,axis=1))
