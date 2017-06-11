a43bb1d695169de526f85f8e0dae2f91c543a602
RBigley <wx8ball@gmail.com>
2017-06-11 08:32:16 -0600

    fix: Update function for all chores
    The function works for all chores.  A list of lists is created for
    chores that all much perform.

--- program/chores.py
+++ program/chores.py
@@ -3,43 +3,28 @@ import numpy as np
 import random
 
 chore = pd.read_csv('../data/chores.csv',sep=',')
+chore.sort_values('randomAssigned', ascending=True, inplace=True)
 helpers = pd.read_csv('../data/helpers.csv',sep=',')
 
 hCount = (helpers['helper'].count())
 
-allchore = chore.query("randomAssigned == 'F'")
-allchore.reset_index()
-otherChore = chore.query("randomAssigned == 'T'")
-otherChore.reset_index()
-oChoreCount = otherChore['chore'].count()
-sumChores = otherChore['weeklyFrequency'].sum()
+choreCount = chore['chore'].count()
+sumChores = chore['weeklyFrequency'].sum()
 chorePerHelper = sumChores/hCount
 
-print(oChoreCount)
+print(choreCount)
 print(sumChores)
 print(chorePerHelper)
 
-t = {}
 
-def addAllChores():
-    k = [[] for i in range(7)]
-    for index,row in allchore.iterrows():
-        if row[1] == 7:
-            for p in range(0,7):
-                k[p].append(row[0])
-        else:
-            r = random.sample(range(7), k = row[1])
-            for rnum in r:
-            	k[rnum].append(row[0])
-    return k
-
-def addOtherChores(row):
+def choreAssign(row):
     dayOfChore = random.sample(range(7), k = row['weeklyFrequency'])
     minAge = row['minAge']
     c = row['chore']
     helperGroup = helpers.query('age >= @minAge')
     helperGroup.reset_index()
     helpersCount = helperGroup['age'].count()
+    print(row['chore'])
     wchore = [[] for i in range(7)]
     if row['dayofWeek'] != '*':
         rsplit = row['dayofWeek'].split(';')
@@ -49,18 +34,21 @@ def addOtherChores(row):
             wchore[dChore] = helperGroup['helper'][helpRandom]  
     elif row['weeklyFrequency'] == 7:
         for i in range(7):
-            helpRandom = random.randint(0,helpersCount-1)
-            wchore[i] = helperGroup['helper'][helpRandom]
+            if row['randomAssigned'] == 'T':
+                helpRandom = random.randint(0,helpersCount-1)
+                wchore[i] = helperGroup['helper'][helpRandom]
+            else:
+                wchore[i] = [h for h in helperGroup['helper']]
     else:
         for i in dayOfChore:
-            helpRandom = random.randint(0,helpersCount-1)
-            wchore[i] = helperGroup['helper'][helpRandom]
+            if row['randomAssigned'] == 'T':
+                helpRandom = random.randint(0,helpersCount-1)
+                wchore[i] = helperGroup['helper'][helpRandom]
+            else:
+                wchore[i] = [h for h in helperGroup['helper']]
+    chartDict[row['chore']] = wchore
+
     return wchore
 
-for j in helpers['helper']:
-    t[j] = addAllChores()
 
-    #addOtherChores()
-    #t[j].update(addOtherChores())
-
-print(otherChore.apply(addOtherChores,axis=1))
+print(chore.apply(choreAssign,axis=1))
