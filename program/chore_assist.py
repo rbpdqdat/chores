8995ff1b271debf4543873d25f2dd8a155aed6c9
RBigley <wx8ball@gmail.com>
2017-09-28 22:36:50 -0600

    Feat: Adding class structure to simplify program

---
+++ program/chore_assist.py
@@ -0,0 +1,32 @@
+class chore:
+    'Defining chore object'
+    def __init__(self, name, frequency, min_age, assigned_to, \
+            assigned = False,  day_of_week = '*'):
+        self.name = name
+        self.frequency = frequency
+        self.day_of_week = day_of_week
+        self.min_age = min_age
+        self.assigned_to = assignedto
+        self.assigned = assigned
+
+class helper:
+    ' Defining the helper object'
+    def __init__(self, name, age, max_chores = 5):
+        self.name = name
+        self.age = age
+        self.max_chores = max_chores
+
+
+class adult(Helper):
+    def __init__(self, name, age):
+        pass
+
+class child(Helper):
+    def __init__(self, name, age):
+        pass
+
+class choreChart:
+    ' choreChart represents the empty 7X7 numpy matrix'
+    def __init__(self, name):
+        self.name = name
+        self.weekchart = np.empty([7,7],dtype=object)
